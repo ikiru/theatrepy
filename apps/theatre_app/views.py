7bd17a09c2fcc31620b68f9da7807ace9c80b76c
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-06-25 18:40:42 -0500

    added master css

--- apps/theatre_app/views.py
+++ apps/theatre_app/views.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 def index(request):
     print 'Inside the the index method'
     return render(request,'frontend/index.html')
+
+def about(request):
+    print 'Inside the the index method'
+    return render(request,'frontend/about.html')
