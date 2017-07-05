a34ce4746f723bdd54f05511730acbc05df9db5f
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-05 08:17:36 -0500

    bootstrap added

--- apps/theatre_app/urls.py
+++ apps/theatre_app/urls.py
@@ -3,6 +3,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^about$', views.about),
-    url(r'^profile$', views.profile)
+    url(r'^profile$', views.profile),
+    url(r'^login$', views.login),
+    url(r'^register$', views.register),
 
-    ]
+]
--- apps/theatre_app/views.py
+++ apps/theatre_app/views.py
@@ -4,15 +4,31 @@ from __future__ import unicode_literals
 from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+
+
 def index(request):
     print 'Inside the the index method'
-    return render(request,'frontend/main.html')
+    return render(request, 'frontend/main.html')
+
 
 def about(request):
     print 'Inside the the about method'
-    return render(request,'frontend/about.html')
+    return render(request, 'frontend/about.html')
+
 
 def profile(request):
     pass
     print 'Inside the the blog method'
-    return render(request,'profile/user_profile.html')
+    return render(request, 'profile/user_profile.html')
+
+
+def login(request):
+    pass
+
+    return render(request, 'profile/login.html')
+
+
+def register(request):
+    pass
+
+    return render(request, 'profile/register.html')
