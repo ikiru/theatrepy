91a5d5ee386b37425ac9eb209c2524e8e07301cf
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-07-27 23:14:07 -0500

    django routes done

--- apps/theatre_app/urls.py
+++ apps/theatre_app/urls.py
@@ -1,27 +1,28 @@
 from django.conf.urls import url, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-from . import views
+from rest_framework import routers
+from tutorial.quickstart import views
 
-# Serializers define the API representation.
-
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-
-
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-
-# Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'school', views.SchoolViewSet)
+router.register(r'district', views.DistrictViewSet)
+router.register(r'state', views.StateViewSet)
+router.register(r'gradyear', views.GradyearViewSet)
+router.register(r'tposition', views.TpositionViewSet)
+router.register(r'trank', views.TrankViewSet)
+router.register(r'tlength', views.TlengthViewSet)
+router.register(r'tcatagory', views.TcatagoryViewSet)
+router.register(r'thonor', views.ThonorViewSet)
+router.register(r'dance', views.DanceViewSet)
+router.register(r'specialskill', views.SpecialSkillViewSet)
+router.register(r'vocaltype', views.VocalTypeViewSet)
+router.register(r'showlist', views.ShowlistViewSet)
+router.register(r'techrolelist', views.TechrolelistViewSet)
+router.register(r'rolelist', views.RolelistViewSet)
+router.register(r'conflict', views.ConflictViewSet)
+router.register(r'conflicteason', views.ConflictViewSet)
+router.register(r'audition', views.AudtionViewSet)
+router.register(r'publidher', views.PublisherViewSet)
+router.register(r'venue', views.VenueViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
