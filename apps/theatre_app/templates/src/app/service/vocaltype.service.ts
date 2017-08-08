d3a1cd3bfc2fe75aab6ec6099ba6a591ad16c4fe
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-08-07 22:31:46 -0500

    vocaltype, showlist

---
+++ apps/theatre_app/templates/src/app/service/vocaltype.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { Http } from "@angular/http";
+import "rxjs";
+
+@Injectable()
+export class VocaltypeService {
+  constructor(private _http: Http) {}
+
+  createVocaltype(vocaltype) {
+    return this._http
+      .post("/vocaltype", vocaltype)
+      .map(data => data.json())
+      .toPromise();
+  }
+  getVocaltype(vocaltype) {
+    return this._http
+      .get("/vocaltype", vocaltype)
+      .map(data => data.json())
+      .toPromise();
+  }
+  updateVocaltype(vocaltype) {
+    return this._http
+      .patch(`/vocaltype/${vocaltype._id}`, vocaltype)
+      .map(data => data.json())
+      .toPromise();
+  }
+
+  destroyVocaltype(id: string) {
+    return this._http
+      .delete(`/vocaltype/${id}`)
+      .map(data => data.json())
+      .toPromise();
+  }
+}
